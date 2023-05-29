1be94f4a9f9aa1ed5b94bd959ec8af2615d373eb
Katarzyna Śliwka <sliwkakatarzyna12@gmail.com>
2023-05-29 10:53:50 +0200

    adding slogan styling

--- components/layout/Slogan.js
+++ components/layout/Slogan.js
@@ -1,5 +1,7 @@
+import classes from "./Slogan.module.css";
+
 function Slogan() {
-  return <h1>Discover Unique Stays, Create Lasting Memories</h1>;
+  return <h1 className={classes.slogan}>Discover Unique Stays, Create Lasting Memories</h1>;
 }
 
 export default Slogan;
