e3dc0b495f11065b1cf2e1777ee6a579f7c7de10
Katarzyna Śliwka <sliwkakatarzyna12@gmail.com>
2023-05-29 12:13:50 +0200

    basic styling detail page

--- components/houses/HouseDetail.js
+++ components/houses/HouseDetail.js
@@ -1,15 +1,32 @@
 import { Fragment } from "react";
 import classes from "./HouseDetail.module.css";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import { GoLocation } from "react-icons/go";
+
 
 function HouseDetail(props) {
   return (
     <Fragment>
-      <img src={props.image} alt={props.title}></img>
-      <h1>{props.title}</h1>
-      <address>{props.location}</address>
-      <h3>{props.price}</h3>
-      <p>{props.rentalAvailability}</p>
-      <p>{props.description}</p>
+      <Container className="mt-4">
+        <Row>
+          <Col md={6} lg={6}>
+            <Image
+              src={props.image}
+              alt={props.title}
+              className={classes.image}
+              fluid
+            ></Image>
+          </Col>
+          <Col md={6} className="mt-5">
+            <h1>{props.title}</h1>
+            <address>
+              <GoLocation /> {props.location}
+            </address>
+            <p>{props.price}</p>
+            <p className="text-muted">{props.description}</p>
+          </Col>
+        </Row>
+      </Container>
     </Fragment>
   );
 }
