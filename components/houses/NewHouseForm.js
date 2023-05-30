809b5fe0c515fc610ee32f58dc1e2a4109b3038b
Katarzyna Śliwka <sliwkakatarzyna12@gmail.com>
2023-05-30 13:32:52 +0200

    form styling

--- components/houses/NewHouseForm.js
+++ components/houses/NewHouseForm.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 import classes from "./NewHouseForm.module.css";
 
@@ -54,61 +55,78 @@ function NewHouseForm(props) {
   };
 
   return (
-    <form className={classes.form} onSubmit={submitHandler}>
-      <div className={classes.control}>
-        <label htmlFor="title">Title</label>
-        <input type="text" required id="title" ref={titleInputRef} />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="image">Image</label>
-        <input type="url" required id="image" ref={imageInputRef} />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="location">Location</label>
-        <input type="text" required id="location" ref={locationInputRef} />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="rentalAvailability">Select Dates</label>
-        <Calendar
-          onChange={handleCalendarChange}
-          value={dateRange}
-          selectRange={true}
-          id="rentalAvailability"
-          aria-label="Rental Availability"
-        />
-      </div>
-      {dateRange.length > 0 ? (
-        <p className="text-center">
-          <span className="bold">Start:</span> {formatDate(dateRange[0])}
-          &nbsp;|&nbsp;
-          <span className="bold">End:</span> {formatDate(dateRange[1])}
-        </p>
-      ) : (
-        <p className="text-center">
-          <span className="bold">Default selected date:</span>{" "}
-          {formatDate(dateRange[0])}
-        </p>
-      )}
-      <label htmlFor="price">Price</label>
-      <div className={`input-group ${classes.control}`}>
-        <div className="input-group-prepend">
-          <span className="input-group-text">$</span>
-        </div>
-        <input type="text" required className="form-control" id="price" ref={priceRef} />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="description">Description</label>
-        <textarea
-          id="description"
-          required
-          rows="5"
-          ref={descriptionInputRef}
-        ></textarea>
-      </div>
-      <div className={classes.actions}>
-        <button>Add</button>
-      </div>
-    </form>
+    <Container>
+      <Row>
+        <Col md={6}>
+          <form className={classes.form} onSubmit={submitHandler}>
+            <div className={classes.control}>
+              <label htmlFor="title">Title</label>
+              <input type="text" required id="title" ref={titleInputRef} />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="image">Image</label>
+              <input type="url" required id="image" ref={imageInputRef} />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="location">Location</label>
+              <input
+                type="text"
+                required
+                id="location"
+                ref={locationInputRef}
+              />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="rentalAvailability">Select Dates</label>
+              <Calendar
+                onChange={handleCalendarChange}
+                value={dateRange}
+                selectRange={true}
+                id="rentalAvailability"
+                aria-label="Rental Availability"
+              />
+            </div>
+            {dateRange.length > 0 ? (
+              <p className="text-center">
+                <span className="bold">Start:</span> {formatDate(dateRange[0])}
+                &nbsp;|&nbsp;
+                <span className="bold">End:</span> {formatDate(dateRange[1])}
+              </p>
+            ) : (
+              <p className="text-center">
+                <span className="bold">Default selected date:</span>{" "}
+                {formatDate(dateRange[0])}
+              </p>
+            )}
+            <label htmlFor="price">Price</label>
+            <div className={`input-group ${classes.control}`}>
+              <div className="input-group-prepend">
+                <span className="input-group-text">$</span>
+              </div>
+              <input
+                type="text"
+                required
+                className="form-control"
+                id="price"
+                ref={priceRef}
+              />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="description">Description</label>
+              <textarea
+                id="description"
+                required
+                rows="5"
+                ref={descriptionInputRef}
+              ></textarea>
+            </div>
+            <div className={classes.formButton}>
+            <Button >Add</Button>
+            </div>
+          </form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
